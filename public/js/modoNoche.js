d60181e2bff45199e2a58de29397953c8d8950b4
raul <git config --global user.email 1832859@alu.murciaeduca.es>
2025-02-12 13:54:45 +0100

    ultima version?

---
+++ public/js/modoNoche.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleDarkModeButton = document.getElementById("toggleDarkMode");
+    const body = document.body;
+
+    // Comprobar si el usuario ya tiene el modo oscuro activado
+    if (localStorage.getItem("darkMode") === "enabled") {
+        body.classList.add("dark-mode");
+        toggleDarkModeButton.textContent = "Modo Día";
+    }
+
+    toggleDarkModeButton.addEventListener("click", function () {
+        body.classList.toggle("dark-mode");
+        if (body.classList.contains("dark-mode")) {
+            localStorage.setItem("darkMode", "enabled");
+            toggleDarkModeButton.textContent = "Modo Día";
+        } else {
+            localStorage.setItem("darkMode", "disabled");
+            toggleDarkModeButton.textContent = "Modo Noche";
+        }
+    });
+});
